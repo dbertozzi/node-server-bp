ff9978bf82c9440b8180358de278d10814d7ec76
dbertozzi <david.a.bertozzi@gmail.com>
2020-09-21 12:35:30 -0400

    Added logging and seperate prod webpack config
    Added log4js for event logging, and morgan for
    request logs.
    
    Added production config for webpack.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import express from "express";
+import dotenv from "dotenv"
+import { logger } from "./utils/logger"
+import requestLogger from "morgan"
+import morgan from "morgan";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+
+app.use(morgan("tiny"))
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.listen(port, () => logger.info(`App listening on port ${port}!`));
